e6b78bd04db50b8f88176caa99ebbd604074c735
Jagjeet Singh <jagjeet7136@gmail.com>
2023-06-20 15:23:53 +0530

    finish-UpdateTask-and-adding-UpdateTaskCss

--- todo-list-react/src/components/task/UpdateTask.js
+++ todo-list-react/src/components/task/UpdateTask.js
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
+import styles from "./UpdateTask.module.css";
+import moment from "moment";
+import axios from "axios";
 
 export const UpdateTask = () => {
+    const enteredTaskTitle = useRef("");
+    const enteredNotes = useRef("");
+    const enteredDueDate = useRef();
+    const enteredReminder = useRef();
+    const [isFormValid, setIsFormValid] = useState(true);
+    const [errorMessage, setErrorMessage] = useState("");
+
     const location = useLocation();
     const taskProps = location.state.task;
     console.log(taskProps);
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let expiryDateTemp = enteredDueDate.current.value;
+        let reminderDateTemp = enteredReminder.current.value;
+
+        if (expiryDateTemp === null || expiryDateTemp.trim() === "") {
+            expiryDateTemp = "";
+        } else {
+            expiryDateTemp = moment(new Date(enteredDueDate.current.value)).format(
+                "DD-MM-YYYY"
+            );
+        }
+
+        if (reminderDateTemp === null || reminderDateTemp.trim() === "") {
+            reminderDateTemp = "";
+        } else {
+            reminderDateTemp = moment(new Date(enteredReminder.current.value)).format(
+                "DD-MM-YYYY HH:MM:SS"
+            );
+        }
+
+        const newTask = {
+            id: taskProps.id,
+            taskTitle: enteredTaskTitle.current.value,
+            notes: enteredNotes.current.value,
+            expiryDate: expiryDateTemp,
+            reminder: reminderDateTemp,
+        };
+
+        axios
+            .patch("http://localhost:2222/task", newTask)
+            .then((res) => {
+                if (!isFormValid) {
+                    setIsFormValid(true);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                let caughtErrorMessage = "Some error occured!";
+                if (error.response != null && error.response.data != null) {
+                    caughtErrorMessage = error.response.data.message.toLowerCase();
+                    if (caughtErrorMessage.includes("nothing")) {
+                        caughtErrorMessage = "no field is updated!";
+                    }
+                }
+                setIsFormValid(false);
+                setErrorMessage(caughtErrorMessage);
+            });
+        enteredTaskTitle.current.value = "";
+        enteredNotes.current.value = "";
+        enteredDueDate.current.value = null;
+        enteredReminder.current.value = null;
+
+    }
 
     return (<div className="project">
         <div className="container">
@@ -12,21 +78,31 @@ export const UpdateTask = () => {
                 <div className="col-md-8 m-auto">
                     <h5 className="display-4 text-center">Update Project form</h5>
                     <hr />
-                    <form>
+                    <form onSubmit={onSubmitHandler}>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg " placeholder="Project Name" />
+                            <input type="text" className="form-control form-control-lg " placeholder="Task Name" ref={enteredTaskTitle} />
                         </div>
 
                         <div className="form-group">
-                            <textarea className="form-control form-control-lg" placeholder="Project Notes"></textarea>
+                            <textarea className="form-control form-control-lg" placeholder="Task Notes" ref={enteredNotes}></textarea>
                         </div>
                         <h6>Due Date</h6>
                         <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="start_date" />
+                            <input type="date" className="form-control form-control-lg" name="due_date" ref={enteredDueDate} />
                         </div>
                         <h6>Reminder Date</h6>
                         <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="end_date" />
+                            <input type="date" className="form-control form-control-lg" name="reminder" ref={enteredReminder} />
+                        </div>
+
+                        <div
+                            className={
+                                isFormValid
+                                    ? styles.errorMsgContainer
+                                    : styles.errorMsgContainer.active
+                            }
+                        >
+                            <span className={styles.errorMsg}>{errorMessage}</span>
                         </div>
 
                         <input type="submit" className="btn btn-primary btn-block mt-4" />
