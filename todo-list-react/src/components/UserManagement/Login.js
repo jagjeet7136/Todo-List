cffdbf58cc90cc19b167c64b405443d63f01ed7c
Jagjeet Singh <jagjeet7136@gmail.com>
2023-07-03 14:14:10 +0530

    adding-AuthContext-and-login-code

--- todo-list-react/src/components/UserManagement/Login.js
+++ todo-list-react/src/components/UserManagement/Login.js
@@ -1,17 +1,44 @@
+import React, { useContext, useRef } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../../context/AuthContext';
+
 export const Login = () => {
+    const authContext = useContext(AuthContext);
+    const username = useRef("");
+    const password = useRef("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const loginObject = {
+            username: username.current.value,
+            password: password.current.value
+        }
+        axios
+            .post("http://localhost:2222/user/login", loginObject)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((error) => {
+
+            });
+
+    };
+
+
     return (
         <div className="login">
             <div className="container">
                 <div className="row">
                     <div className="col-md-8 m-auto">
                         <h1 className="display-4 text-center">Log In</h1>
-                        <form action="dashboard.html">
+                        <form onSubmit={handleLogin}>
                             <div className="form-group">
                                 <input
                                     type="email"
                                     className="form-control form-control-lg"
                                     placeholder="Email Address"
                                     name="email"
+                                    ref={username}
                                 />
                             </div>
                             <div className="form-group">
@@ -20,6 +47,7 @@ export const Login = () => {
                                     className="form-control form-control-lg"
                                     placeholder="Password"
                                     name="password"
+                                    ref={password}
                                 />
                             </div>
                             <input type="submit" className="btn btn-info btn-block mt-4" />
---
+++ todo-list-react/src/context/AuthContext.js
@@ -0,0 +1,38 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [token, setToken] = useState('');
+
+    // Check if token exists in localStorage on component mount
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        if (storedToken) {
+            setToken(storedToken);
+            setLoggedIn(true);
+        }
+    }, []);
+
+    const login = (newToken) => {
+        setToken(newToken);
+        setLoggedIn(true);
+        localStorage.setItem('token', newToken);
+    };
+
+    const logout = () => {
+        setToken('');
+        setLoggedIn(false);
+        // Remove token from localStorage
+        localStorage.removeItem('token');
+    };
+
+    return (
+        <AuthContext.Provider value={{ loggedIn, token, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export { AuthContext, AuthProvider };
