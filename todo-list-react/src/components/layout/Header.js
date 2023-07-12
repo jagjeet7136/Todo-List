b774d7bf4a4044bd2892c75eef0ef3efc891a5fe
Jagjeet Singh <jagjeet7136@gmail.com>
2023-07-12 14:03:09 +0530

    toggle-navbar-1

--- todo-list-react/src/components/layout/Header.js
+++ todo-list-react/src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import styles from "./Header.module.css";
@@ -7,6 +7,7 @@ import todoIcon from "../../icons/todo-icon.png";
 export const Header = () => {
   const authContext = useContext(AuthContext);
   const navigate = useNavigate();
+  const [isActive, setIsActive] = useState(false);
 
   const logoutHandler = () => {
     authContext.logout();
@@ -19,35 +20,53 @@ export const Header = () => {
     }
   };
 
+  const toggleHandler = () => {
+    setIsActive((prevState) => !prevState);
+  }
+
+  const headerList = authContext.loggedIn ? (
+    <div>
+      <li>
+        <Link to="/dashboard">
+          Dashboard
+        </Link>
+      </li>
+      <li>
+        <Link to="/login" onClick={logoutHandler}>
+          Logout
+        </Link>
+      </li>
+    </div>
+  ) : (
+    <div>
+      <li>
+        <Link to="/register">
+          Sign Up
+        </Link>
+      </li>
+      <li>
+        <Link to="/login">
+          Login
+        </Link>
+      </li>
+    </div>
+  );
+
   return (
     <div className={styles.header}>
       <Link to="/" onClick={mainPageHandler}><img src={todoIcon} alt=""></img></Link>
 
-      <ul className={styles.headerList}>
-        {authContext.loggedIn ? <>
-          <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
-              Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login" onClick={logoutHandler}>
-              Logout
-            </Link>
-          </li></>
-          : <>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">
-                Sign Up
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-          </>}
+      <ul className={`${styles.headerList} ${styles.flex}`}>
+        {headerList}
       </ul>
+      <div className={styles.toggleMenu} onClick={toggleHandler}>
+        <span className={styles.toggleMenuOne}></span>
+        <span className={styles.toggleMenuTwo}></span>
+      </div>
+      <div className={`${styles.navbarMenu} ${isActive ? styles.active : ''}`}>
+        {headerList}
+      </div>
+
     </div>
   );
 };
