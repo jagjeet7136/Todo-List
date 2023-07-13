7a61940572304a753f413cb6c68b8f61075d13c7
Jagjeet Singh <jagjeet7136@gmail.com>
2023-07-13 22:06:17 +0530

    header-mediaQueries

--- todo-list-react/src/components/layout/Header.js
+++ todo-list-react/src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import styles from "./Header.module.css";
@@ -11,6 +11,7 @@ export const Header = () => {
 
   const logoutHandler = () => {
     authContext.logout();
+    toggleHandlerTwo();
   };
 
   const mainPageHandler = (event) => {
@@ -24,49 +25,59 @@ export const Header = () => {
     setIsActive((prevState) => !prevState);
   }
 
+  const toggleHandlerTwo = () => {
+    if (isActive) {
+      setIsActive(false);
+    }
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      const thresholdWidth = 450;
+
+      if (isActive && windowWidth > thresholdWidth) {
+        setIsActive(false);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isActive]);
+
   const headerList = authContext.loggedIn ? (
     <div>
-      <li>
-        <Link to="/dashboard">
-          Dashboard
-        </Link>
-      </li>
-      <li>
-        <Link to="/login" onClick={logoutHandler}>
-          Logout
-        </Link>
-      </li>
+      <Link to="/dashboard" onClick={toggleHandlerTwo}>
+        Dashboard
+      </Link>
+      <Link to="/login" onClick={logoutHandler}>
+        Logout
+      </Link>
+
     </div>
   ) : (
     <div>
-      <li>
-        <Link to="/register">
-          Sign Up
-        </Link>
-      </li>
-      <li>
-        <Link to="/login">
-          Login
-        </Link>
-      </li>
+      <Link to="/register" onClick={toggleHandlerTwo}>
+        Sign Up
+      </Link>
+      <Link to="/login" onClick={toggleHandlerTwo}>
+        Login
+      </Link>
     </div>
   );
 
   return (
     <div className={styles.header}>
       <Link to="/" onClick={mainPageHandler}><img src={todoIcon} alt=""></img></Link>
-
-      <ul className={`${styles.headerList} ${styles.flex}`}>
-        {headerList}
-      </ul>
+      <div className={`${styles.headerList} ${styles.displayNone}`}>{headerList}</div>
       <div className={styles.toggleMenu} onClick={toggleHandler}>
         <span className={styles.toggleMenuOne}></span>
         <span className={styles.toggleMenuTwo}></span>
       </div>
       <div className={`${styles.navbarMenu} ${isActive ? styles.active : ''}`}>
-        {headerList}
+        <div className={` ${styles.navbar}`}>{headerList}</div>
       </div>
-
     </div>
   );
 };
