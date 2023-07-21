a34a6090efc66f8199d576043491f8345803032d
Jagjeet Singh <jagjeet7136@gmail.com>
2023-07-21 11:33:35 +0530

    styling changed UpdateTask Component

--- todo-list-react/src/components/task/UpdateTask.js
+++ todo-list-react/src/components/task/UpdateTask.js
@@ -1,120 +1,116 @@
 import React, { useRef, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import styles from "./UpdateTask.module.css";
+import styles from "./AddTask.module.css";
 import moment from "moment";
 import axios from "axios";
 import { Header } from "../layout/Header";
 import icon from "../../icons/newIcon.png";
+import ReactDatePicker from "react-datepicker";
 
 export const UpdateTask = () => {
     const enteredTaskTitle = useRef("");
     const enteredNotes = useRef("");
-    const enteredDueDate = useRef();
-    const enteredReminder = useRef();
     const [isFormValid, setIsFormValid] = useState(true);
-    const [errorMessage, setErrorMessage] = useState("");
     const location = useLocation();
     const taskProps = location.state?.props;
+    const [message, setMessage] = useState("");
+    const [taskUpdated, setTaskUpdated] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     if (!taskProps) {
         return <Navigate to="/dashboard" />;
     }
     const token = localStorage.getItem("token");
 
     const onSubmitHandler = (event) => {
+        if (!isFormValid) {
+            setIsFormValid(true);
+        }
+        if (taskUpdated) {
+            setTaskUpdated(false);
+        }
         event.preventDefault();
+        let expiryDateTemp = selectedDate;
 
-        let expiryDateTemp = enteredDueDate.current.value;
-        let reminderDateTemp = enteredReminder.current.value;
-
-        if (expiryDateTemp === null || expiryDateTemp.trim() === "") {
+        if (!expiryDateTemp) {
             expiryDateTemp = "";
         } else {
-            expiryDateTemp = moment(new Date(enteredDueDate.current.value)).format(
+            expiryDateTemp = moment(new Date(selectedDate)).format(
                 "DD-MM-YYYY"
             );
         }
-
-        if (reminderDateTemp === null || reminderDateTemp.trim() === "") {
-            reminderDateTemp = "";
-        } else {
-            reminderDateTemp = moment(new Date(enteredReminder.current.value)).format(
-                "DD-MM-YYYY HH:MM:SS"
-            );
-        }
-
         const newTask = {
             id: taskProps.id,
             taskTitle: enteredTaskTitle.current.value,
             notes: enteredNotes.current.value,
             expiryDate: expiryDateTemp,
-            reminder: reminderDateTemp,
         };
 
         axios
             .patch("http://localhost:2222/task", newTask, { headers: { Authorization: token } })
             .then((res) => {
-                if (!isFormValid) {
-                    setIsFormValid(true);
-                }
+                setTaskUpdated(true);
+                setMessage("Task Updated Successfully");
+                enteredTaskTitle.current.value = "";
+                enteredNotes.current.value = "";
+                setSelectedDate(null);
             })
             .catch((error) => {
                 console.log(error);
                 let caughtErrorMessage = "Some error occured!";
-                if (error.response != null && error.response.data != null && error.response.data.message != null) {
+                if (error.response && error.response.data && error.response.data.message) {
                     caughtErrorMessage = error.response.data.message.toLowerCase();
                     if (caughtErrorMessage.includes("nothing")) {
-                        caughtErrorMessage = "no field is updated!";
+                        caughtErrorMessage = "No field updated!";
                     }
                 }
                 setIsFormValid(false);
-                setErrorMessage(caughtErrorMessage);
+                setMessage(caughtErrorMessage);
             });
-        enteredTaskTitle.current.value = "";
-        enteredNotes.current.value = "";
-        enteredDueDate.current.value = null;
-        enteredReminder.current.value = null;
 
     }
 
-    return (<div className="project">
+    return (<div>
         <Header textColor="greenText" icon={icon} />
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8 m-auto">
-                    <h5 className="display-4 text-center">Update Project form</h5>
-                    <hr />
-                    <form onSubmit={onSubmitHandler}>
-                        <div className="form-group">
-                            <input type="text" className="form-control form-control-lg " placeholder="Task Name" ref={enteredTaskTitle} />
-                        </div>
-
-                        <div className="form-group">
-                            <textarea className="form-control form-control-lg" placeholder="Task Notes" ref={enteredNotes}></textarea>
-                        </div>
-                        <h6>Due Date</h6>
-                        <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="due_date" ref={enteredDueDate} />
-                        </div>
-                        <h6>Reminder Date</h6>
-                        <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="reminder" ref={enteredReminder} />
-                        </div>
-
-                        <div
-                            className={
-                                isFormValid
-                                    ? styles.errorMsgContainer
-                                    : styles.errorMsgContainer.active
-                            }
-                        >
-                            <span className={styles.errorMsg}>{errorMessage}</span>
-                        </div>
-
-                        <input type="submit" className="btn btn-primary btn-block mt-4" />
-                    </form>
-                </div>
+        <form className={styles.form} onSubmit={onSubmitHandler}>
+            <h1 className={styles.formHeading}>Update task</h1>
+            <input type="text" placeholder="Task name" ref={enteredTaskTitle} className={styles.taskName} />
+            <textarea placeholder="Task Notes" ref={enteredNotes} className={styles.taskNotes}></textarea>
+            <div className={`${styles.dateContainer} ${isFormValid ? styles.dateContainerMargin : styles.dateContainerNoMargin} 
+          ${!taskUpdated ? styles.dateContainerMargin : styles.dateContainerNoMargin}`}>
+                <ReactDatePicker
+                    className={styles.formDate}
+                    selected={selectedDate}
+                    onChange={handleDateChange}
+                    placeholderText="Expiry date"
+                    dateFormat="dd-MM-yyyy"
+                    showMonthDropdown
+                    showYearDropdown
+                    dropdownMode="select"
+                    value={selectedDate ? selectedDate.toLocaleDateString('en-GB') : ''}
+                />
+                <span className={styles.calendarIcon}>&#128197;</span>
             </div>
-        </div>
-    </div>
-    );
+            <div className={`${styles.successMsgContainer}
+                        ${taskUpdated ? styles.successMsgContainer + " " + styles.active : ""}`}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLXFtnDBVOuZqvGW2E-Px5DdU8XU9nSoE9dg&usqp=CAU" alt=""></img>
+                <h6 className={styles.successMsg}>{message}</h6>
+            </div>
+            <div className={`${styles.errorMessageContainer}
+                     ${!isFormValid ? styles.errorMessageContainer + " " + styles.active : ""}`}>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9YISfL4Lm8FJPRneGwEq8_-
+                        9Nim7YeuMJMw&usqp=CAU"
+                    alt=""></img>
+                <h6 className={styles.errorMessage}>{message}</h6>
+            </div>
+            <input
+                type="submit"
+                className={styles.formButton}
+            />
+        </form>
+    </div>);
 }
